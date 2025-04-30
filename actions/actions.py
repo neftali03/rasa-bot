ac5c04e82e3ca6eabdd22f2356208e861fa4e1f8
neftali <neftali@depotelas.com>
2025-04-30 16:52:53 -0600

    Add new details

--- actions/actions.py
+++ actions/actions.py
@@ -20,3 +20,22 @@ class ActionCheckUserReady(Action):
             dispatcher.utter_message(text="No entendí tu respuesta, ¿puedes repetirla?")
 
         return []
+
+class ActionCheckUserStatus(Action):
+    def name(self) -> Text:
+        return "action_check_user_status"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_status = tracker.get_slot("user_status")
+
+        if user_status == "true":
+            dispatcher.utter_message(response="utter_user_status_ok")
+        elif user_status == "false":
+            dispatcher.utter_message(response="utter_user_status_not_yet")
+        else:
+            dispatcher.utter_message(text="No entendí tu respuesta, ¿puedes repetirla?")
+
+        return []
