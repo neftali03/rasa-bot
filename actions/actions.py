1f6695ca5714b0c3cec0e3c7aceda95b51de2b92
neftali <neftali@depotelas.com>
2025-05-23 14:35:34 -0600

    Add endpoint to Laravel from Rasa

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,22 @@
+import uvicorn
+from db_connection import fetch_questions
+from fastapi import FastAPI, Request
 from rasa_sdk import Action
 
-from actions.db_connection import fetch_questions
+app = FastAPI()
+
+
+@app.post("/saludo")
+async def saludar_usuario(request: Request):
+    """Get the greeting from laravel."""
+    data = await request.json()
+    nombre = data.get("nombre", "desconocido")
+    saludo = f"Hola {nombre}, ¿cómo estás? rasa"
+    return {"saludo": saludo}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5055)
 
 
 class ActionListQuestions(Action):
